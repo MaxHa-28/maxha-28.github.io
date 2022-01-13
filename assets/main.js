1d09040fd7e3cce9fc0c8e7a11af31febfbb23c2
MaxHa-28 <maxhajek@outlook.de>
2022-01-13 22:18:31 +0100

    add prod files

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,1 @@
-(() => {
-  // src/index.js
-  if (localStorage.theme === "dark" || !("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-  document.getElementById("dark-mode-btn").onclick = function() {
-    localStorage.theme = "dark";
-    document.documentElement.classList.add("dark");
-  };
-  document.getElementById("light-mode-btn").onclick = function() {
-    localStorage.theme = "light";
-    document.documentElement.classList.remove("dark");
-  };
-})();
+(()=>{localStorage.theme==="dark"||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark");document.getElementById("dark-mode-btn").onclick=function(){localStorage.theme="dark",document.documentElement.classList.add("dark")};document.getElementById("light-mode-btn").onclick=function(){localStorage.theme="light",document.documentElement.classList.remove("dark")};})();
