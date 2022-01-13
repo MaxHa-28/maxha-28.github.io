7090a76813736d4e32f9d72e072a67adb704e231
MaxHa-28 <maxhajek@outlook.de>
2022-01-13 23:24:49 +0100

    setAttribute for aria-checked

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ if (
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.classList.add("dark");
+  document
+    .getElementById("toggle-dark-mode")
+    .setAttribute("aria-checked", "true");
 } else {
   document.documentElement.classList.remove("dark");
+  document
+    .getElementById("dark-mode-btn")
+    .setAttribute("aria-checked", "false");
 }
 
 document.getElementById("dark-mode-btn").onclick = function () {
   localStorage.theme = "dark";
   document.documentElement.classList.add("dark");
+  document
+    .getElementById("toggle-dark-mode")
+    .setAttribute("aria-checked", "true");
 };
 
 document.getElementById("light-mode-btn").onclick = function () {
   localStorage.theme = "light";
   document.documentElement.classList.remove("dark");
+  document
+    .getElementById("toggle-dark-mode")
+    .setAttribute("aria-checked", "false");
 };
