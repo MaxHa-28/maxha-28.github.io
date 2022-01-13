208226e402f1e0c5d287cf8d859a32e84b797b9c
MaxHa-28 <maxhajek@outlook.de>
2022-01-13 23:59:34 +0100

    fix class name and rework dark mode toggle

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
+let theme;
+
 if (
   localStorage.theme === "dark" ||
   (!("theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.classList.add("dark");
-  document
-    .getElementById("toggle-dark-mode")
-    .setAttribute("aria-checked", "true");
+  theme = true;
 } else {
   document.documentElement.classList.remove("dark");
-  document
-    .getElementById("dark-mode-btn")
-    .setAttribute("aria-checked", "false");
+  theme = false;
 }
 
-document.getElementById("dark-mode-btn").onclick = function () {
-  localStorage.theme = "dark";
-  document.documentElement.classList.add("dark");
-  document
-    .getElementById("toggle-dark-mode")
-    .setAttribute("aria-checked", "true");
-};
+document.getElementById("toggle-dark-mode").setAttribute("aria-checked", theme);
 
-document.getElementById("light-mode-btn").onclick = function () {
-  localStorage.theme = "light";
-  document.documentElement.classList.remove("dark");
+document.getElementById("toggle-dark-mode").onclick = function () {
+  if (theme) {
+    localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+    theme = false;
+  } else {
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+    document
+      .getElementById("toggle-dark-mode")
+      .setAttribute("aria-checked", "true");
+    theme = true;
+  }
+
   document
     .getElementById("toggle-dark-mode")
-    .setAttribute("aria-checked", "false");
+    .setAttribute("aria-checked", theme);
 };
